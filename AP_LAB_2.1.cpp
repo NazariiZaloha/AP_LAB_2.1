00e1ea03a480884c52654b4cf991bd0d1479dd21
unknown <zaloganazar048@gmail.com>
2025-09-21 20:49:28 +0300

    commit branch_main

--- AP_LAB_2.1.cpp
+++ AP_LAB_2.1.cpp
@@ -5,13 +5,14 @@ using namespace std;
 int main() {
     double a;
    double z1;
-   // double z2;
+   double z2;
     a = a * M_PI / 180.0;
     cout << "Enter a:" << endl; cin >> a;
     z1 = 1 - 0.25 * (2 * (sin(a) * sin(a))) + 2 * (cos(a));
-    //z2 = (cos(a) * cos(a))  + (cos(a) * cos(a) * cos(a) *cos(a));
+    z2 = (cos(a) * cos(a))  + (cos(a) * cos(a) * cos(a) *cos(a));
 
     cout << "z1:  " << z1 << endl;
-    //cout << "z2:  " << z2 << endl;
+    cout << "z2:  " << z2 << endl;
+    cout << "Correct operation" << endl;
     return 0;
 }
